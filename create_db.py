63537db0cbb0854e43a3f920123fdc5745455b56
BlaineCowen <80175841+BlaineCowen@users.noreply.github.com>
2022-10-03 10:18:32 -0500

    first commit

---
+++ create_db.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import os
+import sqlite3
+
+df1 = pd.DataFrame()
+
+
+#loops of csv files in csv_downloads folder
+def loop_csv():
+    path = 'csv_downloads'
+    files = os.listdir(path)
+    i=0
+    for file in files:
+        if i == 0:
+            df2 = pd.read_csv(f"csv_downloads/{file}", header=2,encoding='latin-1')
+            df_concat = pd.concat([df1, df2], ignore_index=True)
+            print(len(df_concat))
+
+            i+=1
+        else:
+            df2 = pd.read_csv(f"csv_downloads/{file}", header=2,encoding='latin-1')
+            df_concat = pd.concat([df_concat, df2], ignore_index=True)
+            print(len(df_concat))
+    df_concat.to_csv('all_results.csv', index=False)
+
+loop_csv()
