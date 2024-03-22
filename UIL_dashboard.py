1788a67d7d051e1600278c14e370b7ef8a67cdc0
BlaineCowen <80175841+BlaineCowen@users.noreply.github.com>
2024-03-22 16:07:33 -0500

    new public dashboard

--- UIL_dashboard.py
+++ UIL_dashboard.py
@@ -6,14 +6,14 @@ import streamlit as st
 
 def add_song_concat(df):
     score_subset = [
-        "Concert Score 1",
-        "Concert Score 2",
-        "Concert Score 3",
-        "Concert Final Score",
-        "Sight Reading Score 1",
-        "Sight Reading Score 2",
-        "Sight Reading Score 3",
-        "Sight Reading Final Score",
+        "concert_score_1",
+        "concert_score_2",
+        "concert_score_3",
+        "concert_final_score",
+        "sight_reading_score_1",
+        "sight_reading_score_2",
+        "sight_reading_score_3",
+        "sight_reading_final_score",
     ]
 
     # drop any rows where all scores are na
@@ -26,9 +26,9 @@ def add_song_concat(df):
     cols_not_in_subset = df.columns.difference(score_subset)
     df[cols_not_in_subset] = df[cols_not_in_subset].fillna("")
 
-    df["song_concat"] = df["Title 1"] + " " + df["Title 2"] + " " + df["Title 3"]
+    df["song_concat"] = df["title_1"] + " " + df["title_2"] + " " + df["title_3"]
     df["composer_concat"] = (
-        df["Composer 1"] + " " + df["Composer 2"] + " " + df["Composer 3"]
+        df["composer_1"] + " " + df["composer_2"] + " " + df["composer_3"]
     )
     # remove any non-alphanumeric characters and spaces
     df["song_concat"] = df["song_concat"].str.replace(r"[^\w\s]", "")
@@ -41,9 +41,9 @@ def add_song_concat(df):
     df["song_concat"] = df["song_concat"].str.lower()
     df["composer_concat"] = df["composer_concat"].str.lower()
 
-    # director search
-    df["Director_search"] = df["Director"].str.lower()
-    df["Additional Director_search"] = df["Additional Director"].str.lower()
+    # # director search
+    # df["director_search"] = df["director"].str.lower()
+    # df["additional_director_search"] = df["additional_sirector"].str.lower()
 
     # fill na all
 
@@ -57,75 +57,75 @@ def clean_data():
     df = add_song_concat(df)
 
     # fix date
-    df["Contest Date"] = pd.to_datetime(df["Contest Date"], errors="coerce")
+    df["contest_date"] = pd.to_datetime(df["contest_date"], errors="coerce")
 
-    # change contest date to just the year
-    df["Year"] = df["Contest Date"].dt.year.astype(str)
+    # change contest_date to just the year
+    df["year"] = df["contest_date"].dt.year.astype(str)
 
     # remove anywhere where concert score 1 is dna
-    df = df[df["Concert Score 1"] != "DNA"]
-    df = df[df["Concert Score 1"].isna() == False]
-    df = df[df["Concert Score 1"] != "DQ"]
+    df = df[df["concert_score_1"] != "DNA"]
+    df = df[df["concert_score_1"].isna() == False]
+    df = df[df["concert_score_1"] != "DQ"]
 
-    df = df[df["Concert Score 2"] != "DNA"]
-    df = df[df["Concert Score 2"].isna() == False]
-    df = df[df["Concert Score 2"] != "DQ"]
+    df = df[df["concert_score_2"] != "DNA"]
+    df = df[df["concert_score_2"].isna() == False]
+    df = df[df["concert_score_2"] != "DQ"]
 
-    df = df[df["Concert Score 3"] != "DNA"]
-    df = df[df["Concert Score 3"].isna() == False]
-    df = df[df["Concert Score 3"] != "DQ"]
+    df = df[df["concert_score_3"] != "DNA"]
+    df = df[df["concert_score_3"].isna() == False]
+    df = df[df["concert_score_3"] != "DQ"]
 
-    df = df[df["Concert Final Score"] != "DNA"]
-    df = df[df["Concert Final Score"].isna() == False]
-    df = df[df["Concert Final Score"] != "DQ"]
+    df = df[df["concert_final_score"] != "DNA"]
+    df = df[df["concert_final_score"].isna() == False]
+    df = df[df["concert_final_score"] != "DQ"]
 
-    df = df[df["Sight Reading Score 1"] != "DNA"]
-    df = df[df["Sight Reading Score 1"].isna() == False]
-    df = df[df["Sight Reading Score 1"] != "DQ"]
+    df = df[df["sight_reading_score_1"] != "DNA"]
+    df = df[df["sight_reading_score_1"].isna() == False]
+    df = df[df["sight_reading_score_1"] != "DQ"]
 
-    df = df[df["Sight Reading Score 2"] != "DNA"]
-    df = df[df["Sight Reading Score 2"].isna() == False]
-    df = df[df["Sight Reading Score 2"] != "DQ"]
+    df = df[df["sight_reading_score_2"] != "DNA"]
+    df = df[df["sight_reading_score_2"].isna() == False]
+    df = df[df["sight_reading_score_2"] != "DQ"]
 
-    df = df[df["Sight Reading Score 3"] != "DNA"]
-    df = df[df["Sight Reading Score 3"].isna() == False]
-    df = df[df["Sight Reading Score 3"] != "DQ"]
+    df = df[df["sight_reading_score_3"] != "DNA"]
+    df = df[df["sight_reading_score_3"].isna() == False]
+    df = df[df["sight_reading_score_3"] != "DQ"]
 
-    df = df[df["Sight Reading Final Score"] != "DNA"]
-    df = df[df["Sight Reading Final Score"].isna() == False]
-    df = df[df["Sight Reading Final Score"] != "DQ"]
+    df = df[df["sight_reading_final_score"] != "DNA"]
+    df = df[df["sight_reading_final_score"].isna() == False]
+    df = df[df["sight_reading_final_score"] != "DQ"]
 
     # change all concert scores to int
-    df["Concert Score 1"] = df["Concert Score 1"].astype(float).astype(int)
-    df["Concert Score 2"] = df["Concert Score 2"].astype(float).astype(int)
-    df["Concert Score 3"] = df["Concert Score 3"].astype(float).astype(int)
-    df["Concert Final Score"] = df["Concert Final Score"].astype(float).astype(int)
-    df["Sight Reading Score 1"] = df["Sight Reading Score 1"].astype(float).astype(int)
-    df["Sight Reading Score 2"] = df["Sight Reading Score 2"].astype(float).astype(int)
-    df["Sight Reading Score 3"] = df["Sight Reading Score 3"].astype(float).astype(int)
-    df["Sight Reading Final Score"] = (
-        df["Sight Reading Final Score"].astype(float).astype(int)
+    df["concert_score_1"] = df["concert_score_1"].astype(float).astype(int)
+    df["concert_score_2"] = df["concert_score_2"].astype(float).astype(int)
+    df["concert_score_3"] = df["concert_score_3"].astype(float).astype(int)
+    df["concert_final_score"] = df["concert_final_score"].astype(float).astype(int)
+    df["sight_reading_score_1"] = df["sight_reading_score_1"].astype(float).astype(int)
+    df["sight_reading_score_2"] = df["sight_reading_score_2"].astype(float).astype(int)
+    df["sight_reading_score_3"] = df["sight_reading_score_3"].astype(float).astype(int)
+    df["sight_reading_final_score"] = (
+        df["sight_reading_final_score"].astype(float).astype(int)
     )
 
     # add columns called Choice 1, Choice 2, and Choice 3 where title and composer are combined
-    df["Choice 1"] = df["Title 1"] + "–" + df["Composer 1"]
-    df["Choice 2"] = df["Title 2"] + "–" + df["Composer 2"]
-    df["Choice 3"] = df["Title 3"] + "–" + df["Composer 3"]
+    df["choice_1"] = df["title_1"] + "–" + df["composer_1"]
+    df["choice_2"] = df["title_2"] + "–" + df["composer_2"]
+    df["choice_3"] = df["title_3"] + "–" + df["composer_3"]
 
-    df["School Level"] = ""
-    df.loc[df["Conference"].str.contains("A", na=False), "School Level"] = "High School"
-    df.loc[df["Conference"].str.contains("C", na=False), "School Level"] = (
+    df["school_level"] = ""
+    df.loc[df["conference"].str.contains("A", na=False), "school_level"] = "High School"
+    df.loc[df["conference"].str.contains("C", na=False), "school_level"] = (
         "Middle School/JH"
     )
 
     # change classification to title case
-    df["Classification"] = df["Classification"].str.replace("-", " ")
-    df["Classification"] = df["Classification"].str.title()
-    df.loc[df["Classification"].str.contains("Nv", na=False), "Classification"] = (
+    df["classification"] = df["classification"].str.replace("-", " ")
+    df["classification"] = df["classification"].str.title()
+    df.loc[df["classification"].str.contains("Nv", na=False), "classification"] = (
         "Non Varsity"
     )
     # if classification begins with "V" it is Varsit
-    df.loc[df["Classification"].str.contains(r"^V", na=False), "Classification"] = (
+    df.loc[df["classification"].str.contains(r"^V", na=False), "classification"] = (
         "Varsity"
     )
 
@@ -135,32 +135,11 @@ def clean_data():
 def main():
     st.title("UIL Dashboard")
     st.write(
-        "Welcome to the UIL Dashboard. This dashboard is designed to help you track UIL Concert and SR results from the state of Texas."
+        "Welcome to the UIL Dashboard. This dashboard is designed to help track UIL Concert and SR results from the state of Texas."
     )
     st.write("Please select an option from the sidebar to begin.")
-    # remove anywhere where concert score 1 is not a number
+    # remove anywhere where concert_1 is not a number
     df = clean_data()
-
-    song_name_input = st.sidebar.text_input("Enter a song name", "")
-    song_name_input = song_name_input.lower()
-    song_name_input = re.sub(r"\s+", "", song_name_input)
-
-    composer_name_input = st.sidebar.text_input("Enter a composer name", "")
-    composer_name_input = composer_name_input.lower()
-    composer_name_input = re.sub(r"\s+", "", composer_name_input)
-
-    school_select = st.sidebar.multiselect(
-        "Select a school",
-        df["School"].sort_values().unique(),
-        default=[],
-    )
-
-    director_select = st.sidebar.text_input("Enter a director name", "")
-    director_select = director_select.lower()
-
-    if director_select:
-
-        df = df[df["Director_search"].str.contains(director_select, na=False)]
 
     event_select = st.sidebar.selectbox(
         "Select an event",
@@ -170,48 +149,68 @@ def main():
 
     if event_select:
         if "Band" in event_select:
-            df = df[df["Event"].str.contains("Band", na=False)]
+            df = df[df["event"].str.contains("Band", na=False)]
         if "Chorus" in event_select:
-            df = df[df["Event"].str.contains("Chorus", na=False)]
+            df = df[df["event"].str.contains("Chorus", na=False)]
         if "Orchestra" in event_select:
-            df = df[df["Event"].str.contains("Orchestra", na=False)]
+            df = df[df["event"].str.contains("Orchestra", na=False)]
 
         # create new sidebar to select sub events
         sub_event_select = st.sidebar.multiselect(
             "Select a sub event",
-            df["Event"].sort_values().unique(),
+            df["event"].sort_values().unique(),
             default=[],
         )
 
         if sub_event_select:
             df = df[df["Event"].isin(sub_event_select)]
 
-    school_level_select = st.sidebar.selectbox(
-        "Select a school level",
-        df["School Level"].sort_values().unique(),
-        index=None,
+    school_select = st.sidebar.multiselect(
+        "Select a school",
+        df["school"].sort_values().unique(),
+        default=[],
     )
 
+    school_level_select = st.sidebar.selectbox(
+        "Select a school level",
+        df["school_level"].sort_values().unique(),
+        index=None,
+    )
+    song_name_input = st.sidebar.text_input("Enter a song name", "")
+    song_name_input = song_name_input.lower()
+    song_name_input = re.sub(r"\s+", "", song_name_input)
+
+    composer_name_input = st.sidebar.text_input("Enter a composer name", "")
+    composer_name_input = composer_name_input.lower()
+    composer_name_input = re.sub(r"\s+", "", composer_name_input)
+
+    # director_select = st.sidebar.text_input("Enter a director name", "")
+    # director_select = director_select.lower()
+
+    # if director_select:
+
+    #     df = df[df["Director_search"].str.contains(director_select, na=False)]
+
     if school_level_select:
-        df = df[df["School Level"].str.contains(school_level_select, na=False)]
+        df = df[df["school_level"].str.contains(school_level_select, na=False)]
 
         conference_select = st.sidebar.multiselect(
             "Select a conference",
-            df["Conference"].sort_values().unique(),
+            df["conference"].sort_values().unique(),
             default=[],
         )
 
         if conference_select:
-            df = df[df["Conference"].isin(conference_select)]
+            df = df[df["conference"].isin(conference_select)]
 
     classification_select = st.sidebar.selectbox(
         "Select a classification",
-        df["Classification"].sort_values().unique(),
+        df["classification"].sort_values().unique(),
         index=None,
     )
 
     if classification_select:
-        df = df[df["Classification"] == classification_select]
+        df = df[df["classification"] == classification_select]
 
     # only show rows where song name is in song_concat
     filter_df = df[df["song_concat"].str.contains(song_name_input, na=False)]
@@ -225,15 +224,15 @@ def main():
 
     filter_df = filter_df[
         [
-            "Year",
-            "School",
-            "Director",
-            "Additional Director",
-            "Choice 1",
-            "Choice 2",
-            "Choice 3",
-            "Concert Final Score",
-            "Sight Reading Final Score",
+            "year",
+            "school",
+            "director",
+            "additional_director",
+            "choice_1",
+            "choice_2",
+            "choice_3",
+            "concert_final_score",
+            "sight_reading_final_score",
         ]
     ]
 
@@ -244,12 +243,14 @@ def main():
     # write len
     st.write("Number of rows:", len(filter_df))
 
-    st.write(df)
+    # st.write(df)
 
-    # write len
-    st.write("Number of rows:", len(df))
+    # # write len
+    # st.write("Number of rows:", len(df))
 
-    st.sidebar.write("This dashboard was created by [Blaine Cowen]")
+    st.sidebar.write(
+        "This dashboard was created by [Blaine Cowen](mailto:blaine.cowen@gmail.com)"
+    )
 
 
 if __name__ == "__main__":
