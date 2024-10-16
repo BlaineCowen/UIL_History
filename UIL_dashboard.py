65970ea1b1843a81ef16c501e0c3d0c5bfe0ca84
BlaineCowen <80175841+BlaineCowen@users.noreply.github.com>
2024-10-15 19:02:43 -0500

    change theme

--- UIL_dashboard.py
+++ UIL_dashboard.py
@@ -119,6 +119,11 @@ def clean_data():
     results_df = results_df[results_df["sight_reading_score_3"] != 0]
     results_df = results_df[results_df["sight_reading_final_score"] != 0]
 
+    # fix the sight reading score above 5 to just 5
+    results_df.loc[
+        results_df["sight_reading_final_score"] > 5, "sight_reading_final_score"
+    ] = 5
+
     # change all concert scores to int
     results_df["concert_score_1"] = (
         results_df["concert_score_1"].astype(float).astype(int)
@@ -232,14 +237,21 @@ def clean_pml(pml):
     # replace anything that is not a-z with ""
     pml["total_search"] = pml["total_search"].str.replace(r"[^a-zA-Z]", "", regex=True)
 
+    # fill na with 0
+    pml["performance_count"] = pml["performance_count"].fillna(0)
+
     return pml
 
 
 def main():
     st.title("UIL Dashboard")
+
     st.write(
         "Welcome to the UIL Dashboard. This dashboard is designed to help track UIL Concert and Sight Reading results from the state of Texas."
     )
+
+    st.page_link("pages/about.py", label="About the dashboard")
+
     results_df, pml_df = clean_data()
 
     tab1, tab2 = st.tabs(["C&SR Results", "PML"])
@@ -405,17 +417,33 @@ def main():
                         y=scores_over_time_c.values,
                         mode="lines",
                         name="Selected Results",
+                        line=dict(color="#FF4B4B", width=2),
                     ),
                     go.Scatter(
                         x=list(range(year_select[0], year_select[1] + 1)),
                         y=scores_over_time_c2.values,
                         mode="lines",
                         name="All Results",
+                        line=dict(color="#184883", width=2),
                     ),
                 ]
             )
 
             line_chart_c.update_yaxes(autorange="reversed")
+
+            # Update layout to optimize the legend for mobile
+            line_chart_c.update_layout(
+                legend=dict(
+                    orientation="h",  # Horizontal legend
+                    yanchor="bottom",  # Align legend at the bottom
+                    y=0,  # Position the legend at the bottom
+                    xanchor="right",  # Align legend to the right
+                    x=1,  # Position legend to the right
+                    font=dict(size=10),  # Smaller font size
+                    bgcolor="rgba(255, 255, 255, 0.5)",  # Transparent background
+                ),
+                margin=dict(l=20, r=20, t=20, b=20),  # Compact margins
+            )
 
             st.plotly_chart(line_chart_c)
 
@@ -438,17 +466,32 @@ def main():
                         y=scores_over_time_sr.values,
                         mode="lines",
                         name="Selected Results",
+                        line=dict(color="#FF4B4B", width=2),
                     ),
                     py.graph_objs.Scatter(
                         x=list(range(year_select[0], year_select[1] + 1)),
                         y=scores_over_time_sr2.values,
                         mode="lines",
                         name="All Results",
+                        line=dict(color="#184883", width=2),
                     ),
                 ],
             )
 
             line_chart_sr.update_yaxes(autorange="reversed")
+
+            line_chart_sr.update_layout(
+                legend=dict(
+                    orientation="h",  # Horizontal legend
+                    yanchor="bottom",  # Align legend at the bottom
+                    y=0,  # Position the legend at the bottom
+                    xanchor="right",  # Align legend to the right
+                    x=1,  # Position legend to the right
+                    font=dict(size=10),  # Smaller font size
+                    bgcolor="rgba(255, 255, 255, 0.5)",  # Transparent background
+                ),
+                margin=dict(l=20, r=20, t=20, b=20),  # Compact margins
+            )
 
             st.plotly_chart(line_chart_sr, key="line_chart_sr")
 
@@ -845,6 +888,20 @@ def main():
                 title=f"Share of {event_name_select} grade {grade} performances",
             )
 
+            # Update the layout to anchor the legend at the bottom
+            pie_remaining.update_layout(
+                legend=dict(
+                    orientation="h",  # Horizontal legend
+                    yanchor="bottom",  # Align legend at the bottom
+                    y=-0.1,  # Position legend just below the chart
+                    xanchor="center",  # Center the legend horizontally
+                    x=0.5,  # Center the legend in the middle of the chart
+                    font=dict(size=10),  # Optional: Adjust font size if needed
+                    bgcolor="rgba(255, 255, 255, 0.1)",  # Optional: Transparent backgro
+                ),
+                margin=dict(l=20, r=20, t=100, b=50),  # Adjust margins if necessary
+            )
+
             st.plotly_chart(pie_remaining)
 
     st.write(
@@ -852,7 +909,7 @@ def main():
     )
     # create buy me coffee image with link
     st.html(
-        '<a href="https://www.buymeacoffee.com/blainecowen" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style="height: 45px !important;width: 170px !important;" ></a>'
+        '<a href="https://www.buymeacoffee.com/blainecowen" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style="height: 36px !important;width: 136px !important;" ></a>'
     )
 
 
